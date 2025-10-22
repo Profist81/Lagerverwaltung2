dd77d630bcb26f52678c62e8b0729d62a0d6931f
Profist81 <profist19@googlemail.com>
2025-10-22 14:11:02 +0200

    Erste Version Lagerverwaltung hinzugefügt

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "lager-v1";
+const ASSETS = [
+  "/", "/index.html",
+  "/manifest.webmanifest",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith((async () => {
+    const cache = await caches.open(CACHE);
+    const cached = await cache.match(e.request);
+    const fetched = fetch(e.request).then(r => { cache.put(e.request, r.clone()); return r; }).catch(() => cached);
+    return cached || fetched;
+  })());
+});
